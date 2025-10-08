a6aa1abf926ae34f5dd91abe9d918eb097f7aa92
Ibrahim BIDI <zizwar@gmail.com>
2025-10-08 19:16:45 +0000

    feat: Add AdSense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,12 @@ export default function RootLayout({
                 gtag('config', 'G-FNECJ9Z6Z0');
             `}
         </Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5448783245957365"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         {children}
       </body>
     </html>
